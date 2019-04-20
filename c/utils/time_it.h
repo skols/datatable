798d1e53714ee50a1586e81d72df4ff7fc174de5
Oleksiy Kononenko <oleksiy@h2o.ai>
2019-04-19 18:27:22 -0700

    Add timing header file

---
+++ c/utils/time_it.h
@@ -0,0 +1,45 @@
+//------------------------------------------------------------------------------
+// Copyright 2018 H2O.ai
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//------------------------------------------------------------------------------
+#include <chrono>
+#include <string>
+
+class TimeIt {
+  public:
+    using clock = std::chrono::steady_clock;
+    using ms = std::chrono::milliseconds;
+    static constexpr double time_factor = 1000;
+
+    TimeIt(std::string message_in) {
+      t0 = clock::now();
+      message = message_in;
+    }
+
+    ~TimeIt() {
+      clock::time_point t1 = clock::now();
+      int64_t ms_duration = std::chrono::duration_cast<ms>(t1 - t0).count();
+      printf("%s: %.3f [ms]\n", message.c_str(), ms_duration / time_factor);
+    }
+
+  private:
+    clock::time_point t0;
+    std::string message;
+};

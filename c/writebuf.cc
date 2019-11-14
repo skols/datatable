5d3218af70dd70948cff138728876644e4c57c7b
h2o-ops <anmol@h2o.ai>
2019-11-14 02:24:27 -0800

    Add <algorithm> include and explicitely specify std::min<size_t>

--- c/writebuf.cc
+++ c/writebuf.cc
@@ -5,6 +5,7 @@
 //
 // © H2O.ai 2018
 //------------------------------------------------------------------------------
+#include <algorithm>
 #include <cstring>           // std::memcpy
 #include <errno.h>           // errno
 #include "utils/alloc.h"     // dt::realloc
@@ -103,7 +104,7 @@ size_t FileWritableBuffer::prep_write(size_t src_size, const void* src)
   int attempts_remaining = 5;
   size_t written_to_file = 0;
   while (written_to_file < src_size) {
-    size_t bytes_to_write = std::min(src_size - written_to_file, CHUNK_SIZE);
+    size_t bytes_to_write = std::min<size_t>(src_size - written_to_file, CHUNK_SIZE);
     const void* buf = static_cast<const char*>(src) + written_to_file;
     ssize_t r = ::write(fd, buf, bytes_to_write);
     if (r < 0) {
